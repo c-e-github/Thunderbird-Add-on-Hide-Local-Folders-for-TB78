2e34315140df962bf4da3b721c7d21523b514264
John Bieling <john.bieling@gmx.de>
2021-01-08 11:17:34 +0100

    Use windows API to get window which should be manipulated and make API handle multiple windows.

--- background.js
+++ background.js
@@ -1,1 +1,27 @@
-browser.myapi.hidelocalfolder();
+
+// function to hide the local folder in the given main window (if it is a normal main window)
+function hideLocalFolder(window) {
+	if (window.type != "normal")
+		return;
+	
+	// hide local folders for the given window
+	messenger.myapi.hidelocalfolder(window.id);
+}
+
+// register a event listener for newly opened windows, to
+// automatically call hideLocalFolders() for them
+messenger.windows.onCreated.addListener(hideLocalFolder);
+
+
+// run thru all already opened main windows (type = normal) and hide local folders
+// this will take care of all windows already open while the add-on is being installed or
+// activated during the runtime of Thunderbird.
+async function init() {
+	let windows = await messenger.windows.getAll({windowTypes: ["normal"]});
+	for (let window of windows) {
+		hideLocalFolder(window);
+	}
+}
+
+// run init()
+init();
